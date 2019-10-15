567bfc05520650f5cb223a74c7ac7f8f9e2e05ae
Wojtek <golewski.wojciech@outlook.com>
2019-10-15 20:10:14 +0200

    main-table component generated

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ShopRoutingModule } from './shop-routing.module';
+import { MainTableComponent } from './components/main-table/main-table.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainTableComponent
+  ],
   imports: [
     CommonModule,
     ShopRoutingModule
