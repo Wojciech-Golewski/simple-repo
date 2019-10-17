724412cabb3d46997a62a2a767b9809f0e686a89
Wojtek <golewski.wojciech@outlook.com>
2019-10-17 20:00:29 +0200

    new components reflected within the app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainPageComponent } from './core/components/main-page/main-page.component';
 import { NotFoundComponent } from './core/components/not-found/not-found.component';
+import { AboutUsComponent } from './core/components/about-us/about-us.component';
+import { ContactComponent } from './core/components/contact/contact.component';
 
 
 const routes: Routes = [
   {
     path: 'home',
     component: MainPageComponent
+  },
+  {
+    path: 'about-us',
+    component: AboutUsComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
   },
   {
     path: 'shop',
