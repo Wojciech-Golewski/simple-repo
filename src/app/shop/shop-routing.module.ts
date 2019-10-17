c45ee0f1adb40103edea33acf644fa1d6e7570ed
Wojtek <golewski.wojciech@outlook.com>
2019-10-17 19:15:52 +0200

    full setup of html markup performed

--- src/app/shop/shop-routing.module.ts
+++ src/app/shop/shop-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MainTableComponent } from './components/main-table/main-table.component';
+import { ShopMainComponent } from './components/shop-main/shop-main.component';
 
 
 const routes: Routes = [
   {
     path: '',
-    component: MainTableComponent
+    component: ShopMainComponent
   }
 ];
 
