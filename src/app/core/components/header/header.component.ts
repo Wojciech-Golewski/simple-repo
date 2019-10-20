526122c0983b045575faf3051fe1fbc6f624490d
Wojtek <golewski.wojciech@outlook.com>
2019-10-20 19:19:14 +0200

    animations of side-bar and renderer added

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,15 +1,30 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, Renderer2, ViewChild, ElementRef } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('showHide', [
+      transition(':enter', [
+        style({transform: 'translateX(100%)'}),
+        animate('200ms ease-in', style({transform: 'translateX(0%)'}))
+      ]),
+      transition(':leave', [
+        animate('200ms ease-in', style({transform: 'translateX(100%'}))
+      ])
+    ])
+  ]
 })
 export class HeaderComponent {
 
   isMobile: boolean;
   showSideBar: boolean = false;
   screenWidth: number;
+
+  @ViewChild('sideBarButton', { static: false }) sideBarButton: ElementRef;
+  @ViewChild('sideBar', { static: false }) sideBar: ElementRef;
 
   @HostListener('window:resize', ['$event'])
   getScreenWidth(event?: any) {
@@ -25,8 +40,13 @@ export class HeaderComponent {
     }
   }
 
-  constructor() {
+  constructor(private renderer: Renderer2) {
     this.getScreenWidth();
+    this.renderer.listen('window', 'click', (event: Event) => {
+      if (event.target !== this.sideBarButton.nativeElement && event.target !== this.sideBar.nativeElement) {
+        this.showSideBar = false;
+      }
+    })
   }
 
   showSideBarMenu() {
