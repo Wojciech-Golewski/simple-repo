fbf51e2484583e7e9b7664025d6d49bed94e86e4
Wojtek <golewski.wojciech@outlook.com>
2019-10-21 15:41:02 +0200

    shared table component generated

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { MaterialModule } from './material/material.module';
 import { CommonModule } from '@angular/common';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [TableComponent],
   imports: [
     MaterialModule,
     CommonModule
   ],
   exports: [
     MaterialModule,
-    CommonModule
+    CommonModule,
+    TableComponent
   ]
 })
 export class SharedModule { }
