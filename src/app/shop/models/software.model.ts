ecb256caf0ea7a81f6a1ddabd485f184452b4adf
Wojtek <golewski.wojciech@outlook.com>
2019-10-21 16:11:47 +0200

    software model refactored

--- src/app/shop/models/software.model.ts
+++ src/app/shop/models/software.model.ts
@@ -1,8 +1,10 @@
 import { SoftwareType } from '../enums/software-type.enum';
+import { ModuleStrength } from '../enums/module-strength.enum';
 
 export class Software {
     name: string;
     type: SoftwareType;
-    cost: number;
+    moduleStrength: ModuleStrength;
     description: string;
+    cost: number;
 }
