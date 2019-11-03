59978bd2e5e49202d7f253e6144b14abde8ecdfc
Wojtek <golewski.wojciech@outlook.com>
2019-11-03 19:50:28 +0100

    Added initial code for a very basic API to check connections from Angular app and enabled CORS

--- SimpleRepo.API/Startup.cs
+++ SimpleRepo.API/Startup.cs
@@ -26,6 +26,15 @@ namespace SimpleRepo.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("FullyOpenForTests", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +50,8 @@ namespace SimpleRepo.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("FullyOpenForTests");
 
             app.UseEndpoints(endpoints =>
             {
