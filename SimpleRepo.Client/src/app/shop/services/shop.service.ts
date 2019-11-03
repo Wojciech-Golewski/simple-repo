46b42e5fdd8d4e1bf4a829da3f5c5ede86f5a801
Wojtek <golewski.wojciech@outlook.com>
2019-11-03 19:49:05 +0100

    added constant apiUrl to the service

--- SimpleRepo.Client/src/app/shop/services/shop.service.ts
+++ SimpleRepo.Client/src/app/shop/services/shop.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { HttpClient } from '@angular/common/http';
 
+const apiShopUrl = 'https://localhost:44354/api/shop';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,6 +12,6 @@ export class ShopService {
   constructor(private http: HttpClient) { }
 
   getAllData(): Observable<any> {
-    return this.http.get<any>('');
+    return this.http.get<any>(apiShopUrl);
   }
 }
