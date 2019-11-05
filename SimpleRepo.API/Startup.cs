a660acc7f5ba0c1ed1cb7830b20d0dd8990a60e4
Wojtek <golewski.wojciech@outlook.com>
2019-11-05 18:14:39 +0100

    Code cleanup

--- SimpleRepo.API/Startup.cs
+++ SimpleRepo.API/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SimpleRepo.Repositories.Data;
-using SimpleRepo.Services.Services;
 
 namespace SimpleRepo.API
 {
